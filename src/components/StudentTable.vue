96d17ec9072f42a56922973d71bfac51ce1b7b73
B33 <my@email.com>
2019-10-30 13:42:18 -0500

    trouble shooting studentMessage

---
+++ src/components/StudentTable.vue
@@ -0,0 +1,48 @@
+<template> <div>
+<div class="card student-list m-2 p-2">
+	<h4 class="card-title">Students</h4>
+	<div id="student-table">
+		<table class="table">
+			<tr>
+				<th>Name</th>
+				<th>StarID</th>
+				<th>Present?</th>
+			</tr>
+			<tr class="student-row" v-for="student in students" v-bind:class="'present-'+student.present">
+				<td>{{ student.name }} </td>
+				<td>{{ student.starID }} </td>
+				<td><input type="checkbox" v-model="student.present" v-on:change="checked(student)"></td>
+			</tr>
+		</table>
+	</div>
+</div>
+</div></template>
+<script>
+	export default{
+		name: "StudentTable",
+		props: {
+			students: Array
+		},
+		methods: {
+			checked(student){
+				this.$emit("student-present", student)
+				console.log("box check")
+			}
+		}
+	}
+</script>
+<style>
+	#studnet-table{
+		max-hieght: 500px;
+		overflow: scroll;
+	}
+	
+	.present-true{
+		color: gray;
+		font-style: italic;
+	}
+	
+	.present-false{
+		font-weight: bold;
+	}
+</style>
